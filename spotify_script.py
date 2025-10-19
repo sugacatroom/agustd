80bf52cf109cfeca5865874a18ac851fb49860f2
sugacatroom <ddaygoodday0309@gmail.com>
2025-10-19 11:17:18 +0900

    Update spotify_script.py

--- spotify_script.py
+++ spotify_script.py
@@ -10,11 +10,26 @@ client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
 
 # Agust DのSpotifyアーティストID
-artist_id = '2auC28zjQyVTsiZKNgPRGs'
+artist_ids = ['2auC28zjQyVTsiZKNgPRGs', '0C0XlULifJtAgn6ZNCW2eu']  # Agust D と SUGA
 
 # アルバム一覧を取得
-albums = sp.artist_albums(artist_id, album_type='album,single', limit=50)
-album_ids = [album['id'] for album in albums['items']]
+all_tracks = []
+
+for artist_id in artist_ids:
+    albums = sp.artist_albums(artist_id, album_type='album,single', limit=50)
+    album_ids = [album['id'] for album in albums['items']]
+
+    for album_id in album_ids:
+        tracks = sp.album_tracks(album_id)
+        for track in tracks['items']:
+            track_info = sp.track(track['id'])
+            all_tracks.append({
+                'name': track['name'],
+                'popularity': track_info['popularity'],
+                'album': track_info['album']['name'],
+                'release_date': track_info['album']['release_date']
+            })
+
 
 # トラック情報を取得
 all_tracks = []
